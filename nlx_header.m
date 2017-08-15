04c4e5f2348246469a6d21186abe92dd2eb398ff
Neal Morton <mortonne@gmail.com>
2017-08-15 11:20:23 -0500

    critical fix and docstring

--- nlx_header.m
+++ nlx_header.m
@@ -1,3 +1,6 @@
 function hdr = nlx_header(filename)
+%NLX_HEADER   Read a Neuralynx CSC file header.
+%
+%  hdr = nlx_header(filename)
 
-hdr = Nlx2MatCSC_v3(chan_file{2}, [0 0 0 0 0], 1, 2, [0 0]);
+hdr = Nlx2MatCSC_v3(filename, [0 0 0 0 0], 1, 2, [0 0]);
