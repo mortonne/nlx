403c9ff9b939bb983d74336aa78108bb086fdfc0
Neal Morton <mortonne@gmail.com>
2018-02-22 12:47:00 -0600

    add file existence check

--- nlx_header.m
+++ nlx_header.m
@@ -3,4 +3,8 @@ function hdr = nlx_header(filename)
 %
 %  hdr = nlx_header(filename)
 
+if ~exist(filename, 'file')
+    error('File does not exist: %s', filename);
+end
+
 hdr = Nlx2MatCSC_v3(filename, [0 0 0 0 0], 1, 2, [0 0]);
