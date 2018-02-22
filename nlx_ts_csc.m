5af1dc3f2126579763223908c9354db85a4a9469
Neal Morton <mortonne@gmail.com>
2018-02-22 12:47:50 -0600

    script to load just timestamps from a csc file

---
+++ nlx_ts_csc.m
@@ -0,0 +1,18 @@
+function t = nlx_ts_csc(filename)
+%NLX_TS_CSC   Load time stamps for a CSC data.
+%
+%  t = nlx_ts_csc(filename)
+%
+%  INPUTS
+%  filename - char
+%      Path to .csc file with NeuraLynx data.
+%
+%  OUTPUTS
+%  t - [1 x times] vector
+%      vector of time stamps in microseconds.
+
+if ~exist(filename, 'file')
+    error('File does not exist: %s', filename);
+end
+
+t = Nlx2MatCSC_v3(filename, [1 0 0 0 0], 0, 1);
